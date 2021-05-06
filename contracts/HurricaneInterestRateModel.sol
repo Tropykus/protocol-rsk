d36274b9bceabc82cbda6dcd2f7809846381aaf3
Mesi Rendon <amrendonsa@unal.edu.co>
2021-05-06 17:46:20 -0500

    Hurrican Interest Rate model base data
            We've defined the base parameters and behavior for the
            HurricaneInterestRateModel.

--- contracts/HurricaneInterestRateModel.sol
+++ contracts/HurricaneInterestRateModel.sol
@@ -6,20 +6,26 @@ contract HurricaneInterestRateModel is InterestRateModel {
     using SafeMath for uint256;
 
     address public owner;
-    int256[] public coefficients;
     uint256 public baseBorrowRate;
-    uint256 public slope;
+    uint256 public promisedBaseReturnRate;
+    uint256 public optimalUtilizationRate;
+    uint256 public borrowRateSlope;
+    uint256 public supplyRateSlope;
 
     uint256 constant FACTOR = 1e18;
 
     constructor(
-        int256[] memory _coefficients,
-        uint256 _slope,
-        uint256 _baseBorrowRate
+        uint256 _baseBorrowRate,
+        uint256 _promisedBaseReturnRate,
+        uint256 _optimalUtilizationRate,
+        uint256 _borrowRateSlope,
+        uint256 _supplyRateSlope
     ) public {
-        coefficients = _coefficients;
-        slope = _slope;
         baseBorrowRate = _baseBorrowRate;
+        promisedBaseReturnRate = _promisedBaseReturnRate;
+        optimalUtilizationRate = _optimalUtilizationRate;
+        borrowRateSlope = _borrowRateSlope;
+        supplyRateSlope = _supplyRateSlope;
         owner = msg.sender;
     }
 
@@ -31,19 +37,17 @@ contract HurricaneInterestRateModel is InterestRateModel {
         _;
     }
 
-    function setCoefficients(int256[] memory _coefficients) public onlyOwner {
-        coefficients = _coefficients;
-    }
-
     function getSupplyRate(
         uint256 cash,
         uint256 borrows,
         uint256 reserves,
         uint256 reserveFactorMantissa
     ) public view returns (uint256) {
-        (MathError mError, uint256 exchangeRate) =
-            getExchangeRate(cash, borrows, reserves, 0, reserveFactorMantissa);
-        return exchangeRate;
+        uint256 utilizationRate = utilizationRate(cash, borrows, reserves);
+        return
+            utilizationRate.mul(supplyRateSlope).div(FACTOR).add(
+                promisedBaseReturnRate
+            );
     }
 
     function getBorrowRate(
@@ -52,57 +56,8 @@ contract HurricaneInterestRateModel is InterestRateModel {
         uint256 reserves
     ) public view returns (uint256 borrowRate) {
         uint256 utilizationRate = utilizationRate(cash, borrows, reserves);
-        borrowRate = slope
-            .mul(utilizationRate)
-            .div(FACTOR)
-            .add(baseBorrowRate)
-            .div(blocksPerYear);
-    }
-
-    function getExchangeRate(
-        uint256 _totalCash,
-        uint256 _totalBorrows,
-        uint256 _totalReserves,
-        uint256 _totalSupply,
-        uint256 _reserveFactorMantissa
-    ) public view returns (MathError, uint256) {
-        uint256 utilizationRate =
-            utilizationRate(_totalCash, _totalBorrows, _totalReserves);
-        uint256 borrowRatePerBlock =
-            getBorrowRate(_totalCash, _totalBorrows, _totalReserves);
-        (MathError mError, uint256 polynomial) = getPolynomial(utilizationRate);
-        if (mError != MathError.NO_ERROR) return (mError, 0);
-        uint256 exchangeRate =
-            polynomial
-                .mul(borrowRatePerBlock)
-                .div(FACTOR)
-                .mul(uint256(1e18).sub(_reserveFactorMantissa))
-                .div(FACTOR);
-        return (MathError.NO_ERROR, exchangeRate);
-    }
-
-    function getPolynomial(uint256 _utilizationRate)
-        internal
-        view
-        returns (MathError mError, uint256 polynomial)
-    {
-        uint256 positive;
-        uint256 negative;
-        for (uint256 i = 0; i < coefficients.length; i++) {
-            if (coefficients[i] >= 0) {
-                positive = positive.add(
-                    uint256(coefficients[i]).mul(_utilizationRate**i).div(
-                        FACTOR**i
-                    )
-                );
-            } else {
-                uint256 value = 2**256 - 1 - uint256(coefficients[i]) + 1;
-                negative = negative.add(
-                    value.mul(_utilizationRate**i).div(FACTOR**i)
-                );
-            }
-        }
-        require(positive >= negative, "Regression value is not positive");
-        polynomial = positive.sub(negative);
+        borrowRate = utilizationRate.mul(borrowRateSlope).div(FACTOR).add(
+            baseBorrowRate
+        );
     }
 }
